164306873fa56cb420ee22e4a70756210d098641
vitali.rulinski <vitali.rulinski@altoros.com>
2025-05-03 22:26:00 +0200

    feat: Add booking functionality for members

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -3,10 +3,24 @@
 namespace App\Http\Controllers;
 
 use App\Models\ScheduledClass;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
 use Illuminate\View\View;
 
 class BookingController extends Controller
 {
+    /**
+     * Display a listing of the resource.
+     *
+     * @return View
+     */
+    public function index(): View
+    {
+        $bookings = auth()->user()->bookings()->where('date_time','>',now())->get();
+
+        return view('member.upcoming', compact('bookings'));
+    }
+
     /**
      * Show the form for creating a new booking.
      *
@@ -17,8 +31,28 @@ class BookingController extends Controller
     {
         $scheduledClasses = scheduledClass::where('date_time', '>', now())
             ->with('classType', 'instructor')
-            ->oldest('date_time')->get();
+            ->oldest()->get();
 
         return view('member.book', compact('scheduledClasses'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request): RedirectResponse
+    {
+        auth()->user()->bookings()->attach($request->scheduled_class_id);
+
+        return redirect()->route('booking.index')->with('message', 'Booking created');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(int $id): RedirectResponse
+    {
+        auth()->user()->bookings()->detach($id);
+
+        return redirect()->route('booking.index')->with('message', 'Booking deleted');
     }
 }
